e6c46fd65fa8bd598d5263fb872647866a99071d
Julian Gödde <juliangd02@gmail.com>
2026-09-21 09:17:50 +0200

    feat(karakeep): Backup everything

--- stacks/karakeep/backup/backup-karakeep.sh
+++ stacks/karakeep/backup/backup-karakeep.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 set -e
 
-KARAKEEP_JSON="./karakeep-backup.json"
+STACK_DIR="/home/julian/Desktop/homelab/stacks/karakeep"
+KARAKEEP_DIR="/home/julian/Documents/karakeep-app"
+KARAKEEP_ENV="$STACK_DIR/.env"
+KARAKEEP_COMPOSE="$STACK_DIR/compose.yml"
 BACKUP_PATH="/media/julian/Elements/enc_karakeep-backups"
 
-echo "Backing up Karakeep JSON"
-borg create --stats --verbose --progress "$BACKUP_PATH/karakeep-borg::{now}" "$KARAKEEP_JSON"
+echo "Stopping karakeep compose"
+docker compose --file "$KARAKEEP_COMPOSE" down
+
+echo "Backing up Karakeep data dir, env, and compose file using sudo"
+sudo borg create --stats --verbose --progress "$BACKUP_PATH/karakeep-borg::{now}" \
+    "$KARAKEEP_DIR" "$KARAKEEP_ENV" "$KARAKEEP_COMPOSE"
 echo "Archive created"
+
+echo "Restarting karakeep compose"
+docker compose --file "$KARAKEEP_COMPOSE" up -d
 
 echo "Pruning old backups"
 borg prune --keep-weekly=4 --keep-monthly=3 --progress "$BACKUP_PATH"/karakeep-borg
